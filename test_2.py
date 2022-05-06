3fd666947e3c12d6f8e0ebd42bbfe05438fe4a33
Deeno Gonzalez <deenogonzalez@DeeLuisaas-MBP.attlocal.net>
2022-05-05 22:27:27 -0700

    structured-english

---
+++ test_2.py
@@ -0,0 +1,20 @@
+"""
+The program is trying to determine from the student's grades resulting from the final exam.
+First display the number of grades.
+Second display the average grades.
+Third display the percentage of grades that are above the average grade.
+
+Number of grades: 24
+Average grade: 83.25
+Percentage of grades above average: 54.17%
+
+There will be two functions to calculate the grades.
+
+Function-1 will calculate the percent abover average.
+Function-2 will open the Final.txt file, list the grades, calculate the average, and display the above average total. 
+
+If the amount is above the average of 83.25 then it will display the amount.
+The program will display the number of grades. 
+The program wil display the average grade total. 
+"""
+
